00c97c9f12c0d590be1d380d71b6632cbfdf50d4
Beclamide <john@industrialassets.co.uk>
2018-03-29 09:16:20 +0100

    fixed linting

--- 02-ffi/index.js
+++ 02-ffi/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const ffi = require("ffi");
-const ref = require("ref");
+const ffi = require('ffi');
+const ref = require('ref');
 const int = ref.types.int;
 
 const MyLibrary = ffi.Library('mainlib', {
-  "fibonacci": [int, [int]]
+  'fibonacci': [int, [int]]
 });
 
 MyLibrary.fibonacci.async(40, (error, response) => console.log(response));
